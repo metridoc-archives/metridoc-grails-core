f6c23cc96da9a28e476f60239b841bfdb07cdde7
WuWeizhuo@gmail.com <WuWeizhuo@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-07-13 16:54:21 +0000

    in CreateReport: 1.report warning when given no reportName; 2.formated files created;3.have created controller extends ReportController; 4.created an empty _description.gsp file; 5.use GrailsConsole.info to indicate files created or ignored; 6.if file exists, ask user ifOverWrite by reusing script in ConfigMetridoc; 7.create .css and .js files in corresponding directory.

--- scripts/ConfigMetridoc.groovy
+++ scripts/ConfigMetridoc.groovy
@@ -13,6 +13,9 @@
  * permissions and limitations under the License.
  */
 import org.codehaus.gant.GantBuilder
+import grails.build.logging.GrailsConsole
+
+import static grails.build.logging.GrailsConsole.*
 
 /*
 * Copyright 2010 Trustees of the University of Pennsylvania Licensed under the
@@ -28,13 +31,14 @@ import org.codehaus.gant.GantBuilder
 * or implied. See the License for the specific language governing
 * permissions and limitations under the License.
 */
+//includeTargets << new File("grails.build.logging.GrailsConsole")
 includeTargets << grailsScript("_GrailsInit")
 includeTargets << grailsScript("_GrailsArgParsing")
 
 target(main: "Config metridoc-reports plugin") {
     depends(parseArguments)
-    def copyFileFromMap = new HashMap<File, String>();
-
+    def copyFileFromMap = new HashMap<String, String>();
+    GrailsConsole grailsConsole = new GrailsConsole()
     mapFile(copyFileFromMap, "${basedir}/grails-app/conf/BootStrap.groovy", "${metridocCorePluginDir}/src/templates/conf/BootStrap.groovy")
     mapFile(copyFileFromMap, "${basedir}/grails-app/conf/Config.groovy", "${metridocCorePluginDir}/src/templates/conf/Config.groovy")
     mapFile(copyFileFromMap, "${basedir}/grails-app/conf/DataSource.groovy", "${metridocCorePluginDir}/src/templates/conf/DataSource.groovy")
@@ -42,24 +46,45 @@ target(main: "Config metridoc-reports plugin") {
     mapFile(copyFileFromMap, "${basedir}/grails-app/conf/spring/resources.groovy", "${metridocCorePluginDir}/src/templates/conf/spring/resources.groovy")
 
     copyFileFromMap.each {toFile, fromFile ->
-        def splitName = toFile.name.split("/")
+        def splitName = toFile.split("/")
         def name = splitName[splitName.size()-1]
-        if ((!argsMap['o'])&&(toFile.exists())) {
-            ant.input(addproperty: "ifOverWrite${name}", validargs:"y,n", message: "${toFile} already exists, do you want overwrite it?[y->yes,n->no]:")
-        }
-        if ((argsMap['o'])||(ant.project.getProperty("ifOverWrite${name}") != 'n')) {
-            println "copying ${fromFile} to ${toFile}"
-            ant.mkdir(dir: toFile.getCanonicalPath().replace("/${name}",""))
+        if (argsMap['o']||isFileOverwrite(toFile)){
+            ant.mkdir(dir: toFile.replace("/${name}",""))
             ant.copy(file: "${fromFile}",
-                    tofile: "${toFile.getCanonicalPath()}")
+                    tofile: "${toFile}")
+            grailsConsole.info("copied ${fromFile} to ${toFile}")
         }else{
-            println "Ignored ${name}"
+            grailsConsole.info("Ignored ${name}")
         }
     }
 }
-
-void mapFile(HashMap<File, String> fileMap, String toFile, String fromFile){
-    fileMap.put(new File("${toFile}"), fromFile)
+/**
+ * put toFile and fromFile into a HashMap whose keys are files to copy, and values are files copied from
+ * @param fileMap HashMap record all toFiles and fromFiles
+ * @param toFile filename to which we copy
+ * @param fromFile filename from which we copy
+ */
+void mapFile(HashMap<String, String> fileMap, String toFile, String fromFile){
+    fileMap.put(toFile, fromFile)
+}
+/**
+ * check if given file already exists, if yes, ask user whether overwrite the existing one.
+ * @param filePath path of the file being checked
+ * @return true if the file can be overwrote, false if user choose not to overwrite the existing file
+ */
+isFileOverwrite = {filePath->
+    File file = new File(filePath)
+    String ifOverwrite  = 'n'
+    if (file.exists()){
+        String question = filePath+" already exists, do you want overwrite it?"
+        String[] validArgs = ['y','n']
+        GrailsConsole grailsConsole = new GrailsConsole()
+        ifOverwrite = grailsConsole.userInput(question, validArgs)
+    }
+    if ('n'.equals(ifOverwrite)){
+        return false
+    }
+    return true
 }
 
 setDefaultTarget(main)
--- scripts/CreateReport.groovy
+++ scripts/CreateReport.groovy
@@ -1,107 +1,192 @@
 /*
- * Copyright 2010 Trustees of the University of Pennsylvania Licensed under the
- * Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may
- * obtain a copy of the License at
- *
- * http://www.osedu.org/licenses/ECL-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
+* Copyright 2010 Trustees of the University of Pennsylvania Licensed under the
+* Educational Community License, Version 2.0 (the "License"); you may
+* not use this file except in compliance with the License. You may
+* obtain a copy of the License at
+*
+* http://www.osedu.org/licenses/ECL-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an "AS IS"
+* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
 
 import grails.util.GrailsNameUtils
 import groovy.text.XmlTemplateEngine
 import groovy.text.SimpleTemplateEngine
+import grails.build.logging.GrailsConsole
 
 includeTargets << grailsScript("_GrailsInit")
+includeTargets << new File("scripts/ConfigMetridoc.groovy")
 
 target(main: "create default fileset for user customised report") {
-    String name = argsMap["params"][0]
-    name.toLowerCase()
-    StringBuilder rName = new StringBuilder(name.size())
-    rName.append(Character.toUpperCase(name.charAt(0)))
-    rName.append(name.substring(1))
+    GrailsConsole grailsConsole = new GrailsConsole()
 
-    /*grails-app/conf/metridoc/FooConfig.groovy (empty)*/
-    ant.mkdir(dir: "${basedir}/grails-app/conf/metridoc")
-    ant.touch(file: "${basedir}/grails-app/conf/metridoc/${rName}Config.groovy")
+    if (argsMap["params"].size() > 0) {
+        String name = argsMap["params"][0]
+        name.toLowerCase()
+        StringBuilder rName = new StringBuilder(name.size())
+        rName.append(Character.toUpperCase(name.charAt(0)))
+        rName.append(name.substring(1))
+        def binding = [lowcaseName: "${name}", upcaseName: "${rName}"]
+        def simpleEngine = new groovy.text.SimpleTemplateEngine()
+        def xmlEngine = new groovy.text.XmlTemplateEngine()
+        String fileFullName
 
-    /*grails-app/views/reports/foo/reports/_foo.gsp(empty)  */
-    ant.mkdir(dir: "${basedir}/grails-app/views/reports/${name}")
-    ant.touch(file: "${basedir}/grails-app/views/reports/${name}/_${name}.gsp")
+        /*grails-app/conf/metridoc/FooConfig.groovy (empty)*/
+        fileFullName =  "${basedir}/grails-app/conf/metridoc/${rName}Config.groovy"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/grails-app/conf/metridoc")
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-    /*scripts/RunFoo.groovy(empty)*/
-    ant.mkdir(dir: "${basedir}/scripts")
-    ant.touch(file: "${basedir}/scripts/Run${rName}.groovy")
+        /*grails-app/views/reports/foo/_foo.gsp(empty)
+         *grails-app/views/reports/foo/_description.gsp(empty) */
+        ant.mkdir(dir: "${basedir}/grails-app/views/reports/${name}")
+        fileFullName =  "${basedir}/grails-app/views/reports/${name}/_${name}.gsp"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
+        fileFullName =  "${basedir}/grails-app/views/reports/${name}/_description.gsp"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-    /*grails-app/conf/schemas/foo/foo.changelog-01.xml
-     *grails-app/conf/schemas/foo/fooSchema.xml*/
-    ant.mkdir(dir: "${basedir}/grails-app/conf/schemas/${name}")
-    ant.touch(file: "${basedir}/grails-app/conf/schemas/${name}/${name}.changelog-01.xml")
-    ant.touch(file: "${basedir}/grails-app/conf/schemas/${name}/${name}Schema.xml")
-    def binding = [lowcaseName: "${name}", upcaseName: "${rName}"]
-    def textChangeLog = '''<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
-                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">
-                   </databaseChangeLog>'''
-    def textSchema = '''<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
-                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">
-                   <include file="schemas/\${lowcaseName}/\${lowcaseName}.changelog-01.xml"/>
-                   </databaseChangeLog>'''
-    def xmlEngine = new groovy.text.XmlTemplateEngine()
-    def templateChangeLog = xmlEngine.createTemplate(textChangeLog).make(binding)
-    def templateSchema = xmlEngine.createTemplate(textSchema).make(binding)
-    new File("${basedir}/grails-app/conf/schemas/${name}/${name}Schema.xml").write(templateSchema.toString())
-    new File("${basedir}/grails-app/conf/schemas/${name}/${name}.changelog-01.xml").write(templateChangeLog.toString())
+        /*web-app/foo/css/foo.css(empty)
+         *web-app/foo/js/foo.js(empty) */
 
-    /*grails-app/conf/FooResources.groovy*/
-    ant.mkdir(dir: "${basedir}/grails-app/conf")
-    ant.touch(file: "${basedir}/grails-app/conf/${rName}Resources.groovy")
-    def textInResources = '''modules = {
+        fileFullName =  "${basedir}/web-app/${name}/css/${name}.css"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/web-app/${name}/css")
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
+
+        fileFullName = "${basedir}/web-app/${name}/js/${name}.js"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/web-app/${name}/js")
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
+
+        /*scripts/RunFoo.groovy(empty)*/
+        fileFullName =  "${basedir}/scripts/Run${rName}.groovy"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/scripts")
+            ant.touch(file: fileFullName)
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
+
+        /*grails-app/conf/FooResources.groovy*/
+        fileFullName = "${basedir}/grails-app/conf/${rName}Resources.groovy"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/grails-app/conf")
+            ant.touch(file: fileFullName)
+            def textInResources = '''
+modules = {
     \${lowcaseName} {
         dependsOn 'jquery-ui'
         resource url: '\${lowcaseName}/css/\${lowcaseName}.css'
         resource url: '\${lowcaseName}/js/\${lowcaseName}.js'
     }
 }'''
-    def simpleEngine = new groovy.text.SimpleTemplateEngine()
-    def templateInResource = simpleEngine.createTemplate(textInResources).make(binding)
-    new File("${basedir}/grails-app/conf/${rName}Resources.groovy").write(templateInResource.toString())
+            def templateInResource = simpleEngine.createTemplate(textInResources).make(binding)
+            new File(fileFullName).write(templateInResource.toString())
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-    /*
-    *grails-app/controllers/metridoc/foo/FooController.groovy
-    */
-    ant.mkdir(dir: "${basedir}/grails-app/controllers/metridoc/${name}")
-    ant.touch(file: "${basedir}/grails-app/controllers/metridoc/${name}/${rName}Controller.groovy")
-    def textInController = '''package metridoc.\${lowcaseName}
+        /*grails-app/controllers/metridoc/foo/FooController.groovy*/
+        fileFullName = "${basedir}/grails-app/controllers/metridoc/${name}/${rName}Controller.groovy"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/grails-app/controllers/metridoc/${name}")
+            ant.touch(file: fileFullName)
+            def textInController = '''
+package metridoc.\${lowcaseName}\\n
+import metridoc.ReportController\\n
+class \${upcaseName}Controller extends ReportController{\\n
+    def index() { }\\n
+}'''
+            def templateInController = simpleEngine.createTemplate(textInController).make(binding)
+            new File(fileFullName).write(templateInController.toString())
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-            class \${upcaseName}Controller {
+        /*grails-app/services/metridoc/foo/FooService.groovy*/
+        fileFullName =  "${basedir}/grails-app/services/metridoc/${name}/${rName}Service.groovy"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.mkdir(dir: "${basedir}/grails-app/services/metridoc/${name}")
+            ant.touch(file: fileFullName)
+            def textInService = '''
+package metridoc.\${lowcaseName}\\n
+class \${upcaseName}Service {\\n
+    def serviceMethod() {}\\n
+}'''
+            def templateInService = simpleEngine.createTemplate(textInService).make(binding)
+            new File(fileFullName).write(templateInService.toString())
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
+        /*
+        *grails-app/conf/schemas/foo/foo.changelog-01.xml
+        *grails-app/conf/schemas/foo/fooSchema.xml
+        */
+        ant.mkdir(dir: "${basedir}/grails-app/conf/schemas/${name}")
+        fileFullName = "${basedir}/grails-app/conf/schemas/${name}/${name}.changelog-01.xml"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.touch(file: fileFullName)
+            def textChangeLog = '''
+<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd"/>'''
 
-                def index() { }
-            }'''
-    def templateInController = simpleEngine.createTemplate(textInController).make(binding)
-    new File("${basedir}/grails-app/controllers/metridoc/${name}/${rName}Controller.groovy").write(templateInController.toString())
-    /*
-     * grails-app/services/metridoc/foo/FooService.groovy
-     */
-    ant.mkdir(dir: "${basedir}/grails-app/services/metridoc/${name}")
-    ant.touch(file: "${basedir}/grails-app/services/metridoc/${name}/${rName}Service.groovy")
-    def textInService = '''package metridoc.\${lowcaseName}
+            def templateChangeLog = xmlEngine.createTemplate(textChangeLog).make(binding)
+            new File(fileFullName).write(templateChangeLog.toString())
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-            class \${upcaseName}Service {
+        fileFullName =  "${basedir}/grails-app/conf/schemas/${name}/${name}Schema.xml"
+        if (argsMap['o'] || isFileOverwrite(fileFullName)) {
+            ant.touch(file: fileFullName)
+            def textSchema = '''
+<databaseChangeLog xmlns="http://www.liquibase.org/xml/ns/dbchangelog"
+                   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                   xsi:schemaLocation="http://www.liquibase.org/xml/ns/dbchangelog http://www.liquibase.org/xml/ns/dbchangelog/dbchangelog-2.0.xsd">
+    <include file="schemas/\${lowcaseName}/\${lowcaseName}.changelog-01.xml"/>
+</databaseChangeLog>'''
+            def templateSchema = xmlEngine.createTemplate(textSchema).make(binding)
+            new File(fileFullName).write(templateSchema.toString())
+            grailsConsole.info("Created ${fileFullName}")
+        } else {
+            grailsConsole.info("Ignored ${fileFullName}")
+        }
 
-                def serviceMethod() {
-
-                }
-            }'''
-    def templateInService = simpleEngine.createTemplate(textInService).make(binding)
-    new File("${basedir}/grails-app/services/metridoc/${name}/${rName}Service.groovy").write(templateInService.toString())
-
+    } else {
+        grailsConsole.info("WARN: no report name specified in script 'create-report [reportName]'")
+    }
 }
 
 setDefaultTarget(main)
