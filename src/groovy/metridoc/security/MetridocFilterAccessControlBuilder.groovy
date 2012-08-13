b617027f918f37914d4998490518bae067a3b411
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-08-13 20:04:05 +0000

    added customized security in the config file with fallback and security based on controller, updated the Shiro Security filter to use our new security paradigm, created MetridocFilterAccessControlBuilder to be a more appropriate delegate for custom and fallback closures

---
+++ src/groovy/metridoc/security/MetridocFilterAccessControlBuilder.groovy
@@ -0,0 +1,30 @@
+package metridoc.security
+
+import org.apache.shiro.grails.FilterAccessControlBuilder
+import org.apache.shiro.subject.Subject
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tbarker
+ * Date: 8/13/12
+ * Time: 3:46 PM
+ */
+class MetridocFilterAccessControlBuilder extends FilterAccessControlBuilder{
+
+    def request
+    def response
+    def session
+    def servletContext
+    def flash
+    def params
+    def actionName
+    def controllerName
+    def grailsApplication
+    def applicationContext
+
+    MetridocFilterAccessControlBuilder(Subject subject) {
+        super(subject)
+    }
+
+
+}
