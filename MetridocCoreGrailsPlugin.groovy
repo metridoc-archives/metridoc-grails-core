3efec66dec173111c1955af142717c539e8de043
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-08-17 16:39:05 +0000

    setting the version to 0.50

--- MetridocCoreGrailsPlugin.groovy
+++ MetridocCoreGrailsPlugin.groovy
@@ -16,7 +16,7 @@ import metridoc.schema.SchemaRunner
 */
 class MetridocCoreGrailsPlugin {
     // the plugin version
-    def version = "0.6-SNAPSHOT"
+    def version = "0.50"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.4 > *"
     // the other plugins this plugin depends on
