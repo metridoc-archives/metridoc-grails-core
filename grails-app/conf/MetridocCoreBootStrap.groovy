5de4719bd4dcf897a4beaa9070b8367336fc6328
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-09-06 20:36:41 +0000

    removed the ingestor service that we no longer need

--- grails-app/conf/MetridocCoreBootStrap.groovy
+++ grails-app/conf/MetridocCoreBootStrap.groovy
@@ -3,11 +3,9 @@ class MetridocBootStrap {
 
     def initAuthService
     def quartzService
-    def ingestorInitializerService
 
     def init = { servletContext ->
         initAuthService.init()
         quartzService.scheduleJobs()
-        ingestorInitializerService.init()
     }
 }
