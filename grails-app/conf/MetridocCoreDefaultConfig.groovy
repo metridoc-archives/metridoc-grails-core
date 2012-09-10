2042d99a1199eff2087becdc07802a272dc3ffc9
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-09-10 17:17:46 +0000

    ignoring test-classes in the target folder of metridoc-core, changed the startup project to use the most recent version of metridoc, added some intellij files

--- grails-app/conf/MetridocCoreDefaultConfig.groovy
+++ grails-app/conf/MetridocCoreDefaultConfig.groovy
@@ -1,3 +1,5 @@
+import org.apache.commons.lang.SystemUtils
+
 metridoc {
     schemas {
         admin {
@@ -7,6 +9,23 @@ metridoc {
         user {
             dataSource = "admin"
             schema = "schemas/user/userSchema.xml"
+        }
+    }
+}
+
+
+metridoc {
+    files {
+        directory = "${SystemUtils.USER_HOME}/.metridoc/files"
+        foo {
+            filter  = {
+                it.name.contains("foo")
+            }
+            metaData = {File file ->
+                fileName = file.name
+                beginDate = new Date(file.lastModified())
+                endDate = new Date(file.lastModified())
+            }
         }
     }
 }
