00c837fc0a9cab5a69400cbe1b1bfbd0027e0f4b
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-11-07 18:39:12 +0000

    finished adding camel support to MetridocJob

--- grails-app/jobs/metridoc/test/BarJob.groovy
+++ grails-app/jobs/metridoc/test/BarJob.groovy
@@ -1,10 +1,16 @@
 package metridoc.test
 
 import metridoc.core.MetridocJob
+import org.apache.commons.lang.ObjectUtils
+import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.Processor
+import org.apache.camel.Exchange
 
 
 
 class BarJob extends MetridocJob{
+
+    def someProperty = "somePropertyValue"
     static triggers = {
       simple repeatInterval: 60000l // execute job once every minute
     }
@@ -12,7 +18,45 @@ class BarJob extends MetridocJob{
     @Override
     def doExecute() {
         profile("running bar") {
+            assert grailsApplication
             Thread.sleep(1000)
         }
+
+        assert camelJobContext: "camel context not loaded"
+        assert camelJobContext.registry.lookup("someProperty"): "property not found"
+        assert !camelJobContext.registry.lookup("blah")
+        assert camelJobContext.registry.lookup("quartzScheduler")
+        assert producerJobTemplate
+
+        def camelRouteWorked = false
+        runRoute {
+            from("direct:startBarRoute").process {
+                camelRouteWorked = true
+            }
+        }
+
+        def camelRouteWithRouteBuilderWorked = false
+        runRoute(
+            new RouteBuilder() {
+
+                @Override
+                void configure() {
+                    final processor = new Processor(){
+
+                        @Override
+                        void process(Exchange exchange) {
+                            camelRouteWithRouteBuilderWorked = true
+                        }
+                    }
+                    from("direct:startBarRouteRouteBuilder").process(processor)
+                }
+            }
+        )
+
+        producerJobTemplate.requestBody("direct:startBarRoute", ObjectUtils.NULL)
+        producerJobTemplate.requestBody("direct:startBarRouteRouteBuilder", ObjectUtils.NULL)
+
+        assert camelRouteWorked
+        assert camelRouteWithRouteBuilderWorked
     }
 }
--- src/groovy/metridoc/core/MetridocJob.groovy
+++ src/groovy/metridoc/core/MetridocJob.groovy
@@ -2,28 +2,42 @@ package metridoc.core
 
 import org.apache.camel.builder.RouteBuilder
 import org.quartz.JobExecutionContext
-import org.apache.commons.lang.SystemUtils
+
 import org.slf4j.LoggerFactory
-
-
+import org.apache.camel.ProducerTemplate
+import org.apache.camel.CamelContext
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.spi.Registry
+import metridoc.camel.GroovyRouteBuilder
+import org.apache.camel.impl.DefaultCamelContextNameStrategy
+import metridoc.camel.SqlPlusComponent
+import metridoc.utils.CamelUtils
 
 abstract class MetridocJob {
 
     def concurrent = false
-    def applicationContext
+    def grailsApplication
+    ProducerTemplate producerJobTemplate
+    Registry camelJobRegistry
+    CamelContext camelJobContext
     static final log = LoggerFactory.getLogger(MetridocJob)
 
     def execute(JobExecutionContext context) {
         doExecute(context)
         doExecute()
+        getCamelJobContext().stop()
+        camelJobContext = null
     }
 
     def runRoute(Closure closure) {
-
+        def routeBuilder = new GroovyRouteBuilder()
+        routeBuilder.setRoute(closure)
+        runRoute(routeBuilder)
     }
 
     def runRoute(RouteBuilder builder) {
-
+        getCamelJobContext().addRoutes(builder)
+        CamelUtils.waitTillDone(getCamelJobContext())
     }
 
     def target(Map data, Closure closure) {
@@ -57,5 +71,66 @@ abstract class MetridocJob {
 
     def doExecute(JobExecutionContext context) {
         //override this if job details are required
+    }
+
+    ProducerTemplate getProducerJobTemplate() {
+        if(producerJobTemplate) return producerJobTemplate
+
+        producerJobTemplate = getCamelJobContext().createProducerTemplate()
+    }
+
+    def getCamelJobContext() {
+        if(camelJobContext) return camelJobContext
+        camelJobContext = new DefaultCamelContext(getCamelJobRegistry())
+        camelJobContext.nameStrategy = new DefaultCamelContextNameStrategy("metridocCamelJob")
+        camelJobContext.setLazyLoadTypeConverters(true)
+        camelJobContext.addComponent("sqlplus", new SqlPlusComponent(camelJobContext))
+        camelJobContext.start()
+
+        return camelJobContext
+    }
+
+    def getCamelJobRegistry() {
+        if(camelJobRegistry) return camelJobRegistry
+        final job = this
+        camelJobRegistry = new Registry() {
+
+            @Override
+            Object lookup(String s) {
+                try {
+                    return job."${s}"
+                } catch (MissingPropertyException e) {
+                    return job.grailsApplication.mainContext."${s}"
+                }
+            }
+
+            @Override
+            def <T> T lookup(String s, Class<T> tClass) {
+                Object o = lookup(s);
+
+                try {
+                    if (o) {
+                        return tClass.cast(o);
+                    }
+                } catch (ClassCastException ex) {
+                }
+
+                return null
+            }
+
+            @Override
+            def <T> Map<String, T> lookupByType(Class<T> tClass) {
+                def result = [:]
+                job.metaClass.properties.each {
+                    def propertyName = it.getName()
+                    def lookup = lookup(propertyName, tClass)
+                    if(lookup) {
+                        result[propertyName] = lookup
+                    }
+                }
+
+                return result;
+            }
+        }
     }
 }
