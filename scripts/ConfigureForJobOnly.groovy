7d9bee13cd3ed7713c72e6b5969200d12600e840
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-12-10 21:21:02 +0000

    added some documentation and a script to create a job only project

---
+++ scripts/ConfigureForJobOnly.groovy
@@ -0,0 +1,29 @@
+includeTargets << grailsScript("_GrailsInit")
+
+target(main: "deletes all unused files to make a cleaner project for creating job scripts only") {
+
+    if(grailsAppName == "metridoc-core") {
+        grailsConsole.error "cannot run configuration script in the metridoc-core plugin"
+        exit(-1)
+    }
+
+    grailsConsole.info "deleting directories that are not needed for jobs"
+
+    ant.delete(dir: "grails-app/conf/hibernate")
+    ant.delete(dir: "grails-app/conf/spring")
+    ant.delete(dir: "grails-app/conf/BootStrap.groovy")
+    ant.delete(dir: "grails-app/conf/ApplicationResources.groovy")
+    ant.delete(dir: "grails-app/controllers")
+    ant.delete(dir: "grails-app/services")
+    ant.delete(dir: "grails-app/utils")
+    ant.delete(dir: "grails-app/domain")
+    ant.delete(dir: "grails-app/i18n")
+    ant.delete(dir: "grails-app/taglib")
+    ant.delete(dir: "grails-app/views")
+    ant.delete(dir: "lib")
+    ant.delete(dir: "scripts")
+    ant.delete(dir: "src")
+    ant.delete(dir: "web-app")
+}
+
+setDefaultTarget(main)
