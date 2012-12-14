d6f0ecb1d15a09981576b5101f7bb012d7b087b2
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2012-12-14 01:47:33 +0000

    added javadoc to the MetridocJob and re built the manual

--- src/groovy/metridoc/core/MetridocJob.groovy
+++ src/groovy/metridoc/core/MetridocJob.groovy
@@ -16,20 +16,38 @@ import java.util.concurrent.TimeUnit
 import org.quartz.*
 import org.apache.camel.ServiceStatus
 
+/**
+ * This is the primary class used for running MetriDoc jobs.  Although unnecessary to actually create a periodic job,
+ * it contains helpful methods for routing and profiling tasks, in addition to common triggers.
+ */
 abstract class MetridocJob {
     private static final jobLogger = LoggerFactory.getLogger(MetridocJob)
+    /**
+     * all metridoc jobs are not concurrent by default.  Any needed concurrency should be incorporated into the job \
+     * itself
+     */
     def concurrent = false
     def grailsApplication
 
     private static final MANUAL_RUN_ID_PREFIX = "manual-run"
+
+    /**
+     * unfortunately for quartz to actually register a job, it must have some fire time in the future.  This trigger is
+     * meant to represent a job that is meant to be manually fired off in the scheduling view.  To get quartz to
+     * register the job, t is actually a simple trigger that will wait an arbitrarily long time to fire off, in this
+     * case 20 years.  This will reset everytime the application is restarted
+     */
     static final MANUAL_RUN_TRIGGER = {
         def uuid = UUID.randomUUID().toString()
         def twentyYears = TimeUnit.DAYS.toMillis(365 * 20)
         simple name: "$MANUAL_RUN_ID_PREFIX-$uuid", repeatInterval: 1000, startDelay: twentyYears
     }
 
+    /**
+     * represents a midnight trigger
+     */
     static final MIDNIGHT = {
-        cron cronExpression : "0 0 0 * * ?"
+        cron cronExpression: "0 0 0 * * ?"
     }
 
     private ProducerTemplate _producerJobTemplate
@@ -39,6 +57,12 @@ abstract class MetridocJob {
     private Set _targetsRan = [] as Set
     def Map jobDataMap = [:]
 
+    /**
+     * necessary method that the grails job artifact that quartz will trigger
+     *
+     * @param context
+     * @return
+     */
     def execute(JobExecutionContext context) {
         try {
             jobDataMap = context?.trigger?.jobDataMap
@@ -51,7 +75,7 @@ abstract class MetridocJob {
                 jobLogger.info("target map is $targetMap")
                 def containsDefault = targetMap.containsKey("default")
                 if (containsDefault) {
-                    jobLogger.info "running default"
+                    jobLogger.debug "running default"
                     depends("default")
                 }
             }
@@ -79,6 +103,12 @@ abstract class MetridocJob {
         execute(buildJobContextFacade(target))
     }
 
+    /**
+     * creates a fake
+     *
+     * @param target
+     * @return
+     */
     JobExecutionContext buildJobContextFacade(String target = null) {
         [
                 getJobDetail: {
@@ -104,6 +134,13 @@ abstract class MetridocJob {
         ] as JobExecutionContext
     }
 
+    /**
+     * runs the closure as a route within the {@link GroovyRouteBuilder}.  Really handy for database migrations and
+     * batch processing
+     *
+     * @param closure
+     * @return
+     */
     def runRoute(Closure closure) {
         def routeBuilder = new GroovyRouteBuilder()
         routeBuilder.setRoute(closure)
@@ -115,11 +152,25 @@ abstract class MetridocJob {
         return routeBuilder
     }
 
+    /**
+     * instead of using runRoute with a closure, you can pass a RouteBuilder instead.  This can be convenient if someone
+     * wants to use their own {@link RouteBuilder} or wants to benefit from code completion
+     *
+     * @param builder
+     */
     def runRoute(RouteBuilder builder) {
         getCamelJobContext().addRoutes(builder)
         CamelUtils.waitTillDone(getCamelJobContext())
     }
 
+    /**
+     * creates a target for later execution.  This idea was taken directly from the grails / GANT system and works
+     * similarily
+     *
+     * @param data
+     * @param closure
+     * @return
+     */
     def target(Map data, Closure closure) {
         if (data.size() != 1) {
             throw new JobExecutionException("the map in target can only have one variable, which is the name and the description of the target")
@@ -132,8 +183,14 @@ abstract class MetridocJob {
         targetMap.put(key, closureToRun)
     }
 
+    /**
+     * fires off a target by name if it has not been run yet.  If it has run then it is skipped
+     *
+     * @param targetNames
+     * @return
+     */
     def depends(String... targetNames) {
-        targetNames.each {targetName ->
+        targetNames.each { targetName ->
             Closure target = targetMap.get(targetName)
             if (!target) {
                 throw new JobExecutionException("target $targetName does not exist")
@@ -148,14 +205,18 @@ abstract class MetridocJob {
         }
     }
 
+    /**
+     *
+     * @return all targets that have run
+     */
     Set getTargetsRan() {
         return _targetsRan
     }
-/**
- * profiles a chunk of code stating when it starts and finishes
- * @param description description of the chunk of code
- * @param closure the code to run
- */
+    /**
+     * profiles a chunk of code stating when it starts and finishes
+     * @param description description of the chunk of code
+     * @param closure the code to run
+     */
     def profile(String description, Closure closure) {
         def start = System.currentTimeMillis()
         jobLogger.info "profiling [$description] start"
@@ -164,13 +225,27 @@ abstract class MetridocJob {
         jobLogger.info "profiling [$description] finished ${end - start} ms"
     }
 
+    /**
+     * loads scripts that contain targets to allow for code reuse
+     *
+     * @param scriptClass
+     * @return returns the binding from the script in case global variables need to accessed
+     */
     def includeTargets(Class<Script> scriptClass) {
         return includeTargets(scriptClass, new Binding())
     }
 
+    /**
+     * the same as {@link #includeTargets(java.lang.Class)}, but a binding can be passed so more global variables can
+     * be loaded
+     *
+     * @param scriptClass
+     * @param binding
+     * @return the passed binding
+     */
     def includeTargets(Class<Script> scriptClass, Binding binding) {
 
-        binding.setVariable("target") {Map description, Closure closure ->
+        binding.setVariable("target") { Map description, Closure closure ->
             target(description, closure)
         }
         Script script = scriptClass.newInstance()
@@ -188,6 +263,8 @@ abstract class MetridocJob {
     }
 
     /**
+     *
+     * since each execution is in a new job and the job data map is stored, this is completely unnecessary
      *
      * @deprecated will be deprecated in the future so we can just run doExecute
      */
