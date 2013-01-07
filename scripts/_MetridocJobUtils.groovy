9569edf4d7200d0fbbb105ef194d7a51a05243b6
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-01-07 19:48:22 +0000

    Could not create a metridoc job from another plugin.  Hopefully this fix will take care of that

--- scripts/_MetridocJobUtils.groovy
+++ scripts/_MetridocJobUtils.groovy
@@ -3,9 +3,9 @@ includeTargets << grailsScript("_GrailsCreateArtifacts")
 target(createMetridocJob: "creates a metridoc job based on the name of a passed parameter") {
     depends(checkVersion, parseArguments)
 
-    def type = "Job"
+    def type = "MetridocJob"
     promptForName(type: type)
 
     def name = argsMap["params"][0]
-    createArtifact(name: name, suffix: type, type: type, path: "grails-app/jobs", templatePath: "$metridocCorePluginDir/templates/artifacts/metridoc")
+    createArtifact(name: name, suffix: "Job", type: type, path: "grails-app/jobs")
 }
