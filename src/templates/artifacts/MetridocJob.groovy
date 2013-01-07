ed355115a6fef2702e1152bc08829982f9c03fae
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-01-07 20:05:18 +0000

    added an import for metridoc job so the create job script would work

--- src/templates/artifacts/MetridocJob.groovy
+++ src/templates/artifacts/MetridocJob.groovy
@@ -1,4 +1,7 @@
 @artifact.package@
+
+import metridoc.core.MetridocJob
+
 class @artifact.name@ extends MetridocJob {
     static triggers = {
         simple repeatInterval: 60000l // execute job once in 60 seconds
