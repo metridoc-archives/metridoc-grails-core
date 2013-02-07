40b2e0de44afd329aab51fe4fcea3652c9d33f4b
thomas.p.barker@gmail.com <thomas.p.barker@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2013-02-07 03:31:32 +0000

    removed a unit test that is no longer needed

--- test/unit/metridoc/admin/LogServiceTest.groovy
+++ test/unit/metridoc/admin/LogServiceTest.groovy
@@ -17,14 +17,4 @@ class LogServiceTest {
         def htmlClazz = LogService.getDateClass("2012-08-29 12:48:31,548", null, now)
         assert "all sixHours twelveHours day" == htmlClazz
     }
-
-    @Test
-    void "testing an error that was found"() {
-        def line = "2012-10-17 16:06:52,646 [http-bio-8080-exec-12] ERROR [/metridoc-ezproxy].[grails-errorhandler]  - Allocate exception for servlet grails-errorhandler"
-        def logName = "[/metridoc-ezproxy].[grails-errorhandler]"
-        assert LogService.addATag(line, logName)
-
-        //TODO: currently the produced link does not cause an error anymore.... but now the link doesn't work correctly.  will
-        //fix later
-    }
 }
