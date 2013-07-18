c9180e9877dccc766083bb1cbc860891f48b3faa
Thomas Apicella <tapicella.ms@gmail.com>
2013-07-18 14:40:42 -0400

    Add random mkey
    closes #21

--- grails-app/conf/MetridocCoreBootStrap.groovy
+++ grails-app/conf/MetridocCoreBootStrap.groovy
@@ -1,3 +1,5 @@
+import org.apache.commons.lang.RandomStringUtils
+
 class MetridocCoreBootStrap {
 
     def initAuthService
@@ -6,6 +8,18 @@ class MetridocCoreBootStrap {
     def init = { servletContext ->
         initAuthService.init()
         homeService.bootStrapApplications()
+
+        File testDir = new File(System.getProperty("user.home") + "/.metridoc")
+        File key = new File(System.getProperty("user.home") + "/.metridoc/mkey")
+        if (!testDir.exists()) {
+            testDir.mkdirs();
+        }
+        if (!key.exists()) {
+            log.info "Generating mkey"
+            new File(System.getProperty("user.home") + "/.metridoc/mkey").withWriter { out ->
+                out << RandomStringUtils.randomAlphanumeric(64)
+            }
+        }
     }
 
     def destroy = {
