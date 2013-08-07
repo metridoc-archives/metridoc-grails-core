0c9e998bd3959f23bc584a96e22a01b1f18ccf27
Thomas Apicella <tapicella.ms@gmail.com>
2013-08-07 15:13:23 -0400

    Add login via LDAP server query
    #20

--- grails-app/services/metridoc/core/EncryptionService.groovy
+++ grails-app/services/metridoc/core/EncryptionService.groovy
@@ -6,33 +6,42 @@ import org.jasypt.util.text.StrongTextEncryptor
 class EncryptionService {
     static transactional = false
 
-    def encryptString(String target, Boolean strong) {
-        if (strong) {
+    def encryptString(LdapData data, String target) {
+
+        try {
             StrongTextEncryptor textEncrypt = new StrongTextEncryptor()
             textEncrypt.setPassword(CryptKey.list().get(0).encryptKey)
             String encrypted = textEncrypt.encrypt(target)
-            return encrypted
-        } else {
+            data.encryptedPassword = encrypted
+            data.encryptStrong = true
+
+
+        } catch (org.jasypt.exceptions.EncryptionOperationNotPossibleException ex) {
             BasicTextEncryptor textEncrypt = new BasicTextEncryptor()
             textEncrypt.setPassword(CryptKey.list().get(0).encryptKey)
             String encrypted = textEncrypt.encrypt(target)
-            return encrypted
+            data.encryptedPassword = encrypted
+            data.encryptStrong = false
+
+
         }
 
     }
 
-    def decryptString(String target, Boolean strong) {
-        if (strong) {
+    def decryptString(String target) {
+        try {
             StrongTextEncryptor textEncrypt = new StrongTextEncryptor()
             textEncrypt.setPassword(CryptKey.list().get(0).encryptKey)
             String decrypted = textEncrypt.decrypt(target)
             return decrypted
-        } else {
+
+        } catch (org.jasypt.exceptions.EncryptionOperationNotPossibleException ex) {
             BasicTextEncryptor textEncrypt = new BasicTextEncryptor()
             textEncrypt.setPassword(CryptKey.list().get(0).encryptKey)
             String decrypted = textEncrypt.decrypt(target)
             return decrypted
-        }
 
+        }
     }
 }
+
