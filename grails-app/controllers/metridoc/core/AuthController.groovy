339859c3d75da3d7b3cf2e89d3fdf73e037d8d25
Thomas Apicella <tapicella.ms@gmail.com>
2013-10-22 15:18:30 -0400

    Update copyright

--- grails-app/controllers/metridoc/core/AuthController.groovy
+++ grails-app/controllers/metridoc/core/AuthController.groovy
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2013 Trustees of the University of Pennsylvania Licensed under the
+ * 	Educational Community License, Version 2.0 (the "License"); you may
+ * 	not use this file except in compliance with the License. You may
+ * 	obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing,
+ * 	software distributed under the License is distributed on an "AS IS"
+ * 	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * 	or implied. See the License for the specific language governing
+ * 	permissions and limitations under the License.
+ */
+
+
+
 package metridoc.core
 
 import org.apache.commons.validator.UrlValidator
@@ -6,20 +23,6 @@ import org.apache.shiro.authc.AuthenticationException
 import org.apache.shiro.authc.UsernamePasswordToken
 import org.apache.shiro.web.util.WebUtils
 
-/*
- * Copyright 2010 Trustees of the University of Pennsylvania Licensed under the
- * Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may
- * obtain a copy of the License at
- *
- * http://www.osedu.org/licenses/ECL-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an "AS IS"
- * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
 class AuthController {
 
     def authService
--- grails-app/views/user/_userList.gsp
+++ grails-app/views/user/_userList.gsp
@@ -9,7 +9,24 @@
     </h2>
 
     <div id="createUserForm" hidden="true">
-        <g:form controller="user" action="save" class="form-horizontal">
+        %{--
+  - Copyright 2013 Trustees of the University of Pennsylvania Licensed under the
+  - 	Educational Community License, Version 2.0 (the "License"); you may
+  - 	not use this file except in compliance with the License. You may
+  - 	obtain a copy of the License at
+  -
+  - http://www.osedu.org/licenses/ECL-2.0
+  -
+  - 	Unless required by applicable law or agreed to in writing,
+  - 	software distributed under the License is distributed on an "AS IS"
+  - 	BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+  - 	or implied. See the License for the specific language governing
+  - 	permissions and limitations under the License.
+  --}%
+
+
+
+<g:form controller="user" action="save" class="form-horizontal">
             <div class="control-group">
                 <g:render template="/user/userName"/>
             </div>
